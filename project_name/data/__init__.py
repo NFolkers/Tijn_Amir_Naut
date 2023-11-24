b1af7bb61fd88c1194c614e9973b61c5a23fa234
amabd1212 <amirabdulaziz0112@gmail.com>
2023-11-24 12:01:45 +0100

    Loaded Data and Implemented t-SNE

---
+++ project_name/data/__init__.py
@@ -0,0 +1,31 @@
+import mnist_reader
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+
+X_train, y_train = mnist_reader.load_mnist('/Users/amir0/Documents/MachineLearningPractical/Tijn_Amir_Naut/data', kind='train')
+X_test, y_test = mnist_reader.load_mnist('/Users/amir0/Documents/MachineLearningPractical/Tijn_Amir_Naut/data', kind='t10k')
+
+pca = PCA(n_components=100)
+X_train_pca = pca.fit_transform(X_train)
+
+np.random.seed(0)  
+indices = np.random.choice(X_train_pca.shape[0], 10000, replace=False)
+X_subset = X_train_pca[indices]
+y_subset = y_train[indices]
+
+# Apply t-SNE to the data
+tsne = TSNE(n_components=2, random_state=0)
+X_tsne = tsne.fit_transform(X_subset)
+
+
+plt.figure(figsize=(12, 8))
+for i in range(10):
+    indices = y_subset == i
+    plt.scatter(X_tsne[indices, 0], X_tsne[indices, 1], label=str(i))
+plt.legend()
+plt.title('t-SNE visualization of Fashion MNIST data')
+plt.xlabel('t-SNE feature 1')
+plt.ylabel('t-SNE feature 2')
+plt.show()
